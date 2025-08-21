fd06c5f42791952662bb8cbf6b9a0ef175a81594
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-08-21 20:57:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const parts = url.pathname.split("/").filter(Boolean); // split path → ["", "text", "Hello"]
-    const pathname = parts[0]; // "text" or "image"
-    const msg = parts[1]; // "Hello"
-
+    const pathname = decodeURIComponent(url.pathname.slice(1)); // remove leading '/'
+    const msg = url.searchParams.get("msg"); // get key in '?msg=message'
+    
     // Ask AI by Text
-    if (pathname === "text" && msg) {
+    if (pathname && pathname == "text" && msg) {
       const response = await fetch(`https://text.pollinations.ai/${msg}`);
+      
       return new Response(await response.text(), {
         headers: { "content-type": "text/plain" }
       });
     }
 
     // Generate Image
-    if (pathname === "image" && msg) {
-      const response = await fetch(`https://image.pollinations.ai/prompt/${msg}`);
+    if (pathname && pathname == "image" && msg) {
+      const response = await fetch(`https://image.pollinations.ai/prompt/${encodeURIComponent(msg)}`);
+      
       return new Response(await response.arrayBuffer(), {
-        headers: {
-          "content-type": response.headers.get("content-type") || "image/jpeg",
-        }
+        headers: { "content-type": response.headers.get("content-type") || "image/jpeg", }
       });
     }
-
-    return new Response("Invalid request", { status: 400 });
   }
 }
