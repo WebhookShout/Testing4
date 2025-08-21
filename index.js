239f65d846ab53158e954a36a21b7c014973e4d3
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-08-21 20:18:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export default {
   async fetch(request) {
+    const url = new URL(request.url);
     const pathname = decodeURIComponent(url.pathname.slice(1)); // remove leading '/'
     const msg = url.searchParams.get("msg"); // get key in '?msg=message'
     
