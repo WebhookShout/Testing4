d00222e5c5d622c77e3acf71ccc46408d828a52d
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-09 10:38:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const parts = url.pathname.split("/").filter(Boolean); // e.g. /text/Hello → ["text", "Hello"]
-    const type = parts[0] || null; // "text" or "image"
-    const msg = parts[1] || null;  // "Hello"
-
+    
     // Get IP Address from ipify
     const ipResponse = await fetch("https://api.ipify.org/?format=text");
     const ip = await ipResponse.text();
 
-    return new Response(`Type: ${type}\nMessage: ${msg}\nIP: ${ip}`, {
+    return new Response(`IP: ${ip}`, {
       headers: { "content-type": "text/plain" },
     });
   }
