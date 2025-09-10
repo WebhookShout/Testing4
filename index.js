e931d7c28e6ecd212784bb3629736935265a5832
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-10 20:24:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ export default {
     if (path[0] === "check" && path[1]) {
       const key = path[1];
       
-      return new Response(key, {
+      return new Response(DecodeText(decodeHash(key, ServiceKey), ServiceKey), {
         headers: { "Content-Type": "text/plain" }
       });
     }
