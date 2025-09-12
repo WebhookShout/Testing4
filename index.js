719d4025c75dddabb86e007f5b05c582a8e43cd8
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-12 09:33:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -53,26 +53,7 @@ function DecodeText(encoded, key) {
 
 // Get Date Timestamp function
 function getTimestamp() {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate() + 1).padStart(2, "0");
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  return `${year}${month}${day}${hours}${minutes}${seconds}`;
-}
-
-// Get Current Date Timestamp function
-function getcurrentTimestamp() {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  return `${year}${month}${day}${hours}${minutes}${seconds}`;
+  return Date.now();
 }
 
 
