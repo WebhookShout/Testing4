0844897b6b8def5ec577497cdc6b095e8924ffea
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-12 15:38:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ export default {
         if (Number(DecodeText(githubData[key].message, ServiceKey)) >= getcurrentTimestamp()) {
           valid = true
         }
-      } elseif (key in firebaseData) {
+      } else if (key in firebaseData) {
         if (Number(DecodeText(firebaseData.message, ServiceKey)) >= getcurrentTimestamp()) {
           valid = true
         }
