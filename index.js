b8bacb3377762def62538690a43a75924f2e6912
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-21 02:30:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -104,11 +104,7 @@ export default {
 
     // Make Key Starter
     if (path[0] === "make" && method === "GET") {
-      return Response.redirect(`${domain}/status`, 302, headers: {
-        "content-type": "text/html;charset=UTF-8",
-        "referrer-policy": "no-referrer",
-        "cache-control": "no-store"
-      });
+      return Response.redirect(`${domain}/status`, 302);
     }
     
     // Create Key (always expires in 24h)
